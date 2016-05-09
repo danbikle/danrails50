35c84a57629784673a0998bb2bfe17274f2a0012
Dan Bikle <bikle@bikle.com>
2016-05-09 04:49:50 +0000

    mo

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get  'posts/index'
   root 'posts#index'
+  get ':controller(/:action)'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
