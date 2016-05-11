f5389065df8a09daeb59165063c63cc8871f6af5
Dan Bikle <bikle@bikle.com>
2016-05-11 17:56:55 +0000

    more

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'demo10/starter_template'
 
   get  'posts/index'
-  root 'posts#index'
+  root 'demo10#examples'
   get ':controller(/:action)'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
